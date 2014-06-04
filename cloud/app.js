fc8b1c8d90b3d0670f40be33cbec3e574067ac82
jiakun xu <michael.xu@kintiger.com>
2014-06-04 13:22:43 +0800

    test

--- cloud/app.js
+++ cloud/app.js
@@ -12,11 +12,50 @@ app.get('/hello', function(req, res) {
   res.render('hello', { message: 'Congrats, you just set up your app!' });
 });
 
+var isLegel = function(signature, timestamp, nonce) {
+	var TOKEN = 'xplatform';
+	var arr = [TOKEN, timestamp, nonce];
+	// 对三个参数进行字典序排序
+	arr.sort();
+	// sha1 加密
+	var sha1 = crypto.createHash('sha1');
+	var msg = arr[0] + arr[1] + arr[2];
+	sha1.update(msg);
+	msg = sha1.digest('hex');
+	// 验证
+	if (msg == signature) {
+		console.log('验证成功');
+		return true;
+	} else {
+		console.log('验证失败');
+		return false;
+	}
+};
+
 var url = require('url');
 var querystring = require("querystring");
 
 app.get('/wx', function(req, res) {
-  res.render('wx', { message: querystring.parse(url.parse(req.url).query)["echostr"] });
+			// 获取GET请求的参数
+			var url_params = url.parse(req.url, true);
+
+			console.log(req.url);
+
+			var query = url_params.query;
+
+			res.writeHead(200, {
+						'Content-Type' : 'text/plain'
+					});
+			console.log('Query params:' + query.signature + query.timestamp
+					+ query.nonce);
+
+			if (isLegel(query.signature, query.timestamp, query.nonce)) {
+				// 返回echostr
+				res.end(query.echostr);
+			} else {
+				//
+				res.end('Hello world\n');
+			}
 });
 
 app.get('/sendMsg', function(req, res) {
