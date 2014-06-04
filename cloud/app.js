296d8cccd9f63fe169346817b4d6e1bac4ee3c9c
jiakun xu <michael.xu@kintiger.com>
2014-06-04 13:54:40 +0800

    aaa

--- cloud/app.js
+++ cloud/app.js
@@ -3,14 +3,16 @@ var express = require('express');
 var app = express();
 
 // App 全局配置
-app.set('views','cloud/views');   // 设置模板目录
-app.set('view engine', 'ejs');    // 设置 template 引擎
-app.use(express.bodyParser());    // 读取请求 body 的中间件
+app.set('views', 'cloud/views'); // 设置模板目录
+app.set('view engine', 'ejs'); // 设置 template 引擎
+app.use(express.bodyParser()); // 读取请求 body 的中间件
 
 // 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
 app.get('/hello', function(req, res) {
-  res.render('hello', { message: 'Congrats, you just set up your app!' });
-});
+			res.render('hello', {
+						message : 'Congrats, you just set up your app!'
+					});
+		});
 
 var isLegel = function(signature, timestamp, nonce) {
 	var TOKEN = 'xplatform';
@@ -53,12 +55,14 @@ app.get('/wx', function(req, res) {
 				//
 				res.end('Hello world\n');
 			}
-});
+		});
 
 var url = require('url');
 var querystring = require("querystring");
 
 app.post('/wx', function(req, res) {
+	console.log(req.url);
+	
 	var msg = "<xml><ToUserName><![CDATA["
 			+ querystring.parse(url.parse(req.url).query)["toUserName"]
 			+ "]]></ToUserName><FromUserName><![CDATA[theJiakun]]></FromUserName><CreateTime>12345678</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[你好]]></Content></xml>";
